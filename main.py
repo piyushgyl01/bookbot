ce60123188d51a9fc45a872d7d4ae92fa5cec901
piyush goyal <piyush2022ug@gmail.com>
2025-08-22 07:48:22 +0530

    get_char_count func

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 def get_book_text(path):
     with open(path) as f:
@@ -7,7 +7,10 @@ def get_book_text(path):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
+    char_count = get_char_count(book_text)
+
     print(book_text)
     print(num_words)
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -2,6 +2,19 @@ def get_num_words(text):
     words = text.split()
     count = 0
     for _ in words:
-        count = count + 1
+        count += 1
     
     return f"{count} words found in the document"
+
+def get_char_count(book_text):
+    char_count_dict = {}
+    words_arr = book_text.lower().split()
+
+    for word in words_arr:
+        for char in word:
+            if char in char_count_dict:
+                char_count_dict[char] += 1
+            else:
+                char_count_dict[char] = 1
+
+    return char_count_dict
